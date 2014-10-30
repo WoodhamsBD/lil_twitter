47d04e1d958f6778bcffcefe4311fa6fe5f1445e
woodhamsbd <woodhamsbd#@gmail.com>
2014-10-30 10:49:20 -0700

    migrations

--- db/migrate/20141030104140_create_retweets.rb
+++ db/migrate/20141030104140_create_retweets.rb
@@ -1,4 +1,11 @@
 class CreateRetweets < ActiveRecord::Migration
   def change
+    create_table :retweets do |t|
+      t.integer :original_user
+      t.integer :tweet_id
+      t.integer :retweeter_id
+
+      t.timestamps
+    end
   end
 end
