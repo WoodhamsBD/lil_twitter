6805b5a9456da9f9d9a665afd96170ff623ee746
woodhamsbd <woodhamsbd#@gmail.com>
2014-10-30 12:08:26 -0700

    migration and database set-up, but DAFUQ?

--- app/models/follow.rb
+++
@@ -1,5 +0,0 @@
-class Follow < ActiveRecord::Base
-  # Remember to create a migration!
-  has_many :users
-
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
   has_many :tweets
-  has_many :followers through =>
-  has_many :followees, class_name: "Follow",
-                        foreign_key: ""
 
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id"
+
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
+
+  def following? user
+    self.followeds.include? user
+  end
+
+  def follow user
+    Relationship.create follower_id: self.id, followed_id: user.id
+  end
 end
