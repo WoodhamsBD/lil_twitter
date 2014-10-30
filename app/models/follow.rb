7d11f01f334ffb03e5b8a2378f667e50b3a409b8
woodhamsbd <woodhamsbd#@gmail.com>
2014-10-30 11:08:20 -0700

    models beginning

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,5 @@
 class Follow < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :tweets
+  has_many :followers through =>
+  has_many :followees, class_name: "Follow",
+                        foreign_key: ""
 
 end
