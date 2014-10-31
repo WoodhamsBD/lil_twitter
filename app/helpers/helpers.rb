f066cc60a3823e8e641efddc5d8ed5793f9ce384
Ryan Whiley <whileyryan@gmail.com>
2014-10-30 17:03:11 -0700

    Helpers for followed tweets

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,4 +5,15 @@ helpers do
 		@current_user ||=  User.find(session[:user_id])
 	end
 	
+	def followed_tweets
+		follow_array=[]
+		current_user.followeds.each do |person_you_follow|
+			follow_array << person_you_follow.id
+		end
+		Tweet.where(user_id: follow_array).limit(10).order('id desc')
+	end
+
+	def handle_from_tweet(tweet)
+		User.find(tweet.user_id).handle
+	end
 end
